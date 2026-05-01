c7566fde14ad95632dd48585936b600ca9a99ba5
Gustavo <Gustavo.boiing@gmail.com>
2026-05-01 18:44:34 -0300

    1.5.0 - Criando a classe controller da entidade Contract

---
+++ src/main/java/com/api_horizonte/api_horizonte/Controller/ContractController.java
@@ -0,0 +1,50 @@
+package com.api_horizonte.api_horizonte.Controllers;
+
+import com.api_horizonte.api_horizonte.Business.ContractService;
+import com.api_horizonte.api_horizonte.Infraestructure.DTO.ContractRequest;
+import com.api_horizonte.api_horizonte.Infraestructure.DTO.ContractResponse;
+import com.api_horizonte.api_horizonte.Infraestructure.DTO.ContractUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contracts")
+@RequiredArgsConstructor
+public class ContractController {
+
+    private final ContractService contractService;
+
+    // POST /contracts
+    @PostMapping
+    public ResponseEntity<ContractResponse> createContract(@RequestBody ContractRequest request) {
+        ContractResponse response = contractService.createContract(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    // GET /contracts/user/{cpf}
+    @GetMapping("/user/{cpf}")
+    public ResponseEntity<List<ContractResponse>> findContractByUserCPF(@PathVariable String cpf) {
+        List<ContractResponse> response = contractService.findContractByUserCPF(cpf);
+        return ResponseEntity.ok(response);
+    }
+
+    // GET /contracts/realstate/{name}
+    @GetMapping("/realstate/{name}")
+    public ResponseEntity<List<ContractResponse>> findContractByRealStateName(@PathVariable String name) {
+        List<ContractResponse> response = contractService.findContractByRealStateByName(name);
+        return ResponseEntity.ok(response);
+    }
+
+    // PATCH /contracts/{id}
+    @PatchMapping("/{id}")
+    public ResponseEntity<ContractResponse> updateContract(
+            @PathVariable Integer id,
+            @RequestBody ContractUpdateRequest request) {
+        ContractResponse response = contractService.updateContract(id, request);
+        return ResponseEntity.ok(response);
+    }
+}
